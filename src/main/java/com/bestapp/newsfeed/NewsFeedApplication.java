6b0c2ecf900cf0510ddf2352cfadfe672ddd3006
porovoz <falkon1@list.ru>
2023-08-28 11:17:47 +0300

    initial commit

---
+++ src/main/java/com/bestapp/newsfeed/NewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestapp/newsfeed/NewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestapp.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
